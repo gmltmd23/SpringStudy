5bd7b6f1e508879b4c6aa5f6f3b7d1bad6b4ad93
gmltmd23 <dbsgmltmd23@naver.com>
2021-06-23 23:58:28 +0900

    [SpringStudy] Fix library path 2021/06/22

--- sp5-chap03/src/main/java/config/AppConf1.java
+++ sp5-chap03/src/main/java/config/AppConf1.java
@@ -1,13 +1,12 @@
 package config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import spring.MemberDao;
 import spring.MemberPrinter;
 
 @Configuration
-@Import(AppConf2.class)
 public class AppConf1 {
     @Bean
     public MemberDao memberDao() {
