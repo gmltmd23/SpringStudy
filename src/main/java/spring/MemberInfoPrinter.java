5f61bd49688cd28867fe7d5e34cc3bdba7af518b
unknown <dbsgmltmd23@naver.com>
2021-06-25 17:11:59 +0900

    Create sp5-chap03

---
+++ src/main/java/spring/MemberInfoPrinter.java
@@ -0,0 +1,20 @@
+package spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MemberInfoPrinter {
+    @Autowired
+    private MemberDao memDao;
+    @Autowired
+    private MemberPrinter printer;
+
+    public void printMemberInfo(String email) {
+        Member member = memDao.selectByEmail(email);
+        if (member == null) {
+            System.out.println("멤버정보가 존재하지 않습니다.\n");
+            return;
+        }
+        printer.print(member);
+        System.out.println();
+    }
+}
