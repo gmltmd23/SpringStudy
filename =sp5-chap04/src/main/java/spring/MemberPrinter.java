d684696fe4879b448f34f1929e1b8198ad7bbe96
gmltmd23 <dbsgmltmd23@naver.com>
2021-06-29 23:23:45 +0900

    [SpringStudy] End Autowired 2021/06/29

--- =sp5-chap04/src/main/java/spring/MemberPrinter.java
+++ =sp5-chap04/src/main/java/spring/MemberPrinter.java
@@ -7,19 +7,24 @@ import java.time.format.DateTimeFormatter;
 public class MemberPrinter {
     private DateTimeFormatter dateTimeFormatter;
 
+    public MemberPrinter() {
+        dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일");
+    }
+
     public void print(Member member) {
         if (dateTimeFormatter == null) {
             System.out.printf(
                     "회원 정보 : 아이디 = %d, 이메일 = %s, 이름 = %s, 등록일 = %tF\n",
                     member.getID(), member.getEmail(), member.getName(), member.getRegisterDateTime());
         } else {
+            System.out.println(dateTimeFormatter);
             System.out.printf(
                     "회원 정보 : 아이디 = %d, 이메일 = %s, 이름 = %s, 등록일 = %sF\n",
                     member.getID(), member.getEmail(), member.getName(), dateTimeFormatter.format(member.getRegisterDateTime()));
         }
     }
 
-    @Autowired(required = false)
+    @Autowired(required = false) // 다만 이건 Nullable과는 다른점이 있는데 일치하는 Bean이 없으면 아예 값을 할당하지 않는다. // Nullable은 말그대로 Bean이 없으면 null을 넣는다.
     public void setDateTimeFormatter(DateTimeFormatter dateTimeFormatter) {
         this.dateTimeFormatter = dateTimeFormatter;
     }
