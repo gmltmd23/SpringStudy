e3d9f4520c3374bd0ae3b79aea0fada9ef778a71
gmltmd23 <dbsgmltmd23@naver.com>
2021-07-07 18:02:48 +0900

    [SpringStudy] properties(initMethod, destoryMethod) 2021/07/07

--- src/main/java/executor/Main.java
+++ src/main/java/executor/Main.java
@@ -3,6 +3,7 @@ package executor;
 import config.AppContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.Client;
+import spring.Client2;
 
 public class Main {
     private static AnnotationConfigApplicationContext ctx = null;
@@ -11,6 +12,10 @@ public class Main {
         ctx = new AnnotationConfigApplicationContext(AppContext.class);
         Client temp = ctx.getBean(Client.class);
         temp.send();
+
+        Client2 c2 = ctx.getBean(Client2.class);
+        c2.send();
+
         ctx.close();
     }
 }
