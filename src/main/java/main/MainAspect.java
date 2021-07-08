be87ef30c035117bda82e05251f2d4ce95ceaf73
gmltmd23 <dbsgmltmd23@naver.com>
2021-07-09 00:09:22 +0900

    [SpringStudy] 4.3 @around의 Pointcut 설정과 @pointcut 재사용 2021/07/09

--- src/main/java/main/MainAspect.java
+++ src/main/java/main/MainAspect.java
@@ -10,7 +10,6 @@ public class MainAspect {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
 
         Calculator cal = ctx.getBean("calculator", Calculator.class);
-        //RecCalculator cal = ctx.getBean("calculator", RecCalculator.class);
         long fiveFact = cal.factorial(5);
         System.out.println("cal.factorial(5) = " + fiveFact);
         System.out.println(cal.getClass().getName());
