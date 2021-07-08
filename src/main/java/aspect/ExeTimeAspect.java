15467c6596ad757494e2524318007c0bbc2cd149
gmltmd23 <dbsgmltmd23@naver.com>
2021-07-08 19:02:43 +0900

    [SpringStudy] 4.2 Advice 적용 순서 2021/07/08

--- src/main/java/aspect/ExeTimeAspect.java
+++ src/main/java/aspect/ExeTimeAspect.java
@@ -5,9 +5,12 @@ import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 
 import java.util.Arrays;
+
 @Aspect
+@Order(1)
 public class ExeTimeAspect {
     @Pointcut("execution(* chap07..*(..))")
     private void publicTarget() {
