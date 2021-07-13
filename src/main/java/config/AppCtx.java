ed0ef364e055a97b5c5431935a3e8c65646da489
gmltmd23 <dbsgmltmd23@naver.com>
2021-07-14 00:13:28 +0900

    [SpringStudy] 3.DataSource 설정 2021/07/14

--- src/main/java/config/AppCtx.java
+++ src/main/java/config/AppCtx.java
@@ -1,54 +1,22 @@
-/*
+
 package config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import spring.*;
+import org.apache.tomcat.jdbc.pool.DataSource;
 
 @Configuration
 public class AppCtx {
-    @Bean
-    public MemberDao memberDao() {
-        return new MemberDao();
-    }
 
-    @Bean
-    public MemberRegisterService memberRegSvc() {
-        return new MemberRegisterService(memberDao());
-    }
-
-    @Bean
-    public ChangePasswordService changePwdSvc() {
-        ChangePasswordService pwdSvc = new ChangePasswordService();
-        pwdSvc.setMemberDao(memberDao());
-        return pwdSvc;
-    }
-
-    @Bean
-    public MemberPrinter memberPrinter() {
-        return new MemberPrinter();
-    }
-
-    @Bean
-    public MemberListPrinter listPrinter() {
-        return new MemberListPrinter(memberDao(), memberPrinter());
-    }
-
-    @Bean
-    public MemberInfoPrinter infoPrinter() {
-        MemberInfoPrinter infoPrinter = new MemberInfoPrinter();
-        infoPrinter.setMemberDao(this.memberDao());
-        infoPrinter.setPrinter(this.memberPrinter());
-        return infoPrinter;
-    }
-
-    @Bean
-    public VersionPrinter versionPrinter() {
-        VersionPrinter versionPrinter = new VersionPrinter();
-        versionPrinter.setMajorVersion(5);
-        versionPrinter.setMinorVersion(0);
-        return versionPrinter;
+    @Bean(destroyMethod = "close")
+    public DataSource dataSource() {
+        DataSource ds = new DataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost/spring5fs?characterEncoding=utf8");
+        ds.setUsername("spring5");
+        ds.setPassword("spring5");
+        ds.setInitialSize(2);
+        ds.setMaxActive(10);
+        return ds;
     }
 }
-
- */
--- src/main/java/spring/MemberDao.java
+++ src/main/java/spring/MemberDao.java
@@ -1,29 +1,22 @@
 package spring;
 
-import java.util.*;
+
+import java.util.Collection;
 
 public class MemberDao {
-    private static long nextId = 0;
-    private Map<String, Member> map = new HashMap<String, Member>();
-
     public Member selectByEmail(String email) {
-        return map.get(email);
-    }
-
-    public Collection<Member> selectAll() {
-        ArrayList<Member> temp = new ArrayList<>();
-        for (String key : map.keySet()) {
-            temp.add(map.get(key));
-        }
-        return temp;
+        return null;
     }
 
     public void insert(Member member) {
-        member.setID(++nextId);
-        map.put(member.getEmail(), member);
+
     }
 
     public void update(Member member) {
-        map.put(member.getEmail(), member);
+
+    }
+
+    public Collection<Member> selectAll() {
+        return null;
     }
 }
